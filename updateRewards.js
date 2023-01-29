a28e9a6571c8fd987a9888a91e12f569f857f864
sanketnighot <sanketnighot25@gmail.com>
2023-01-29 17:22:19 +0530

    update

--- updateRewards.js
+++ updateRewards.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 var Web3 = require('web3');
 const {rental_abi} = require('./abi');
 
-const rentalContractAddress = "0x3426042A6eb767493551ECA9Bfe8f3AC85712Bfb"
-const URL = "https://eth-goerli.g.alchemy.com/v2/XvCWKtxc5r_re6uSpNMlqiBDcy5DK-oj";
+const rentalContractAddress = "0xca63b89db5a634ad465927ff63e0fd1495928e23"
+const URL = "https://eth-mainnet.g.alchemy.com/v2/4Yu3Crvr9V7owxJ-msc99y94RTQjKEB-";
 const web3Pro = new Web3(new Web3.providers.HttpProvider(URL));
 
 const RentalContract = new web3Pro.eth.Contract(rental_abi, rentalContractAddress);
 
 const callApi = (num, rewId) => {
-    axios.post('https://rental-api-d5ih3.ondigitalocean.app/api/updateRewards', {init: num, rewardId: rewId}).then((data) => {
+    axios.post('https://rental-api.lordsofthelands.io/api/updateRewards', {init: num, rewardId: rewId}).then((data) => {
         console.log(data.data);
     }).catch((err) => {console.log(err)})   
     console.log(num) 
